86aebcf3d3105f4e17d5c8bfc1e6f93d8ce3f150
Aasvogel Hsu <terry08111@gmail.com>
2019-07-21 12:57:01 +0800

    first commit 201907211256

--- app/src/main/java/com/iii/aspart01/MainActivity.java
+++ app/src/main/java/com/iii/aspart01/MainActivity.java
@@ -3,12 +3,28 @@ package com.iii.aspart01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnlot;
+    private TextView lottext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    btnlot = findViewById(R.id.btn_lottery);
+    lottext = findViewById(R.id.lottery_text);
+
+    btnlot.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.v("b1","ok");
+        }
+    });
+
     }
 }
